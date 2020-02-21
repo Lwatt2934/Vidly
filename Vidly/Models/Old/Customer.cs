49998cac95aa1361e26b2e5d6a0c4a7009c7207e
159350 <larry.watson@fedex.com>
2020-02-21 08:59:01 -0600

    Added for exercise.

--- Vidly/Models/Old/Customer.cs
+++ Vidly/Models/Old/Customer.cs
@@ -1,7 +1,11 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 namespace Vidly.Models
 {
-    public class Movie
+    public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
