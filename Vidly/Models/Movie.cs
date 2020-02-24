8ebff2fc5fd576425f44189d60129fa167bb81ed
159350 <larry.watson@fedex.com>
2020-02-24 15:32:20 -0600

    End of EF section.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 namespace Vidly.Models
 {
     public class Movie
@@ -6,4 +10,6 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
     }
+
+
 }
