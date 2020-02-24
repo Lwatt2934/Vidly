c2cacd9d94fdcb89db877c93663c524751c9cbf2
159350 <larry.watson@fedex.com>
2020-02-24 15:58:10 -0600

    Step 1.

---
+++ Vidly/Migrations/202002242138191_AddMembershipTypeName1.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipTypeName1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.MembershipTypes", "MembershipTypeName", c => c.String(maxLength: 255));
+
+
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.MembershipTypes", "MembershipTypeName", c => c.String());
+        }
+    }
+}
