8062b637905890d25fe030d6e64644b9c2e39f9e
159350 <larry.watson@fedex.com>
2020-02-27 17:00:50 -0600

    Section 4 Final.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -36,7 +36,50 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
+        public ActionResult New()
+        {
+            var genre = _context.Genre.ToList();
+            var viewModel = new NewMovieViewModel
+            {
+                Genre = genre
+            };
+            return View("MovieForm", viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
 
+            if (movie == null)
+                return HttpNotFound();
 
+            var viewModel = new NewMovieViewModel
+            {
+                Movie = movie,
+                Genre = _context.Genre.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+
+                //TryUpdateModel(customerInDb);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,13 +10,16 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
         public Genre Genre { get; set; }
         public byte GenreId { get; set; }
 
-        public DateTime DateAdded { get; set; }
+        public DateTime? DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
         [Range(1, 20)]
