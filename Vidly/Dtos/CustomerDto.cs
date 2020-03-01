6da71f78f42116889d5d18acd21b670eb528e0c0
L TW <lwat413@gmail.com>
2020-03-01 11:07:39 -0600

    Before Exercise...

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+//        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+    }
+}
